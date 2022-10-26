f6d7e8be853ea2595dc16153b63ebe8daf85fa6f
ScottyBH <112198546+ScottyBH@users.noreply.github.com>
2022-10-26 15:33:30 -0700

    added forecast to JS

--- src/script.js
+++ src/script.js
@@ -24,6 +24,41 @@ let day = days[now.getDay()];
 
 today.innerHTML = `${day} ${hours}:${minutes}`;
 
+function getForcast(coordinates) {
+  let apiKey = `894a2e7aa7f46eeca5d8778f6faa5a5b`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  axios.get(apiURL).then();
+  console.log;
+}
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+  <div class="col-sm">
+    <div class="card">
+      <div class="card-body">
+        <p class="card-text">  
+            ${day}
+        </br>
+         <img src="#" alt="Deg" id="icon">
+    </br>
+        <span class="weather-forcast-min">70&deg;</span><span class="weather-forcast-max"> 107&deg;</span>
+    </p>
+      </div>
+    </div>
+  </div>
+`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
 function displayTemperature(response) {
   let temperature = document.querySelector("#today-temp");
   let cityElement = document.querySelector("#city");
@@ -43,6 +78,8 @@ function displayTemperature(response) {
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+  getForcast(tresponse.data.cord);
 }
 
 function search(city) {
@@ -72,6 +109,7 @@ function convertToFehrenheit(event) {
 }
 
 search("Phoenix");
+displayForecast();
 
 let fahrenheitTemp = null;
 
